bf683923024730a68100b21857e3fe5e520ffcad
sck-mtmt <108612924+sck-mtmt@users.noreply.github.com>
2022-08-06 12:26:54 +0000

    ルート確認

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,23 @@
 class BooksController < ApplicationController
   def new
     @book=Book.new
-
   end
 
   def create
     @book=Book.new(book_params)
     if @book.save
+     flash[:notice]="Book was successfully created"
      redirect_to book_path(@book.id)
     else
-     render :new
+     flash[:alret]="Book was not created"
+     @books=Book.all
+     render :index
     end
   end
 
-
   def index
     @books=Book.all
+    @book=Book.new
   end
 
   def show
@@ -24,20 +26,29 @@ class BooksController < ApplicationController
 
   def edit
     @book= Book.find(params[:id])
-
-
   end
 
   def destroy
     book= Book.find(params[:id])
+    Book.destroyd(book_params)
     book.destroy
-    redirect_to '/books'
+    if @book.save
+     redirect_to book_path(@book.id)
+    else
+     flash.now="Book Was Successfully Destroyd"
+     render :edit
+    end
   end
 
   def update
     book= Book.find(params[:id])
     book.update(book_params)
-    redirect_to edit_book_path(book.id)
+    if @book.save
+     redirect_to edit_book_path(book.id)
+    else
+     flash.now="Book Was Successfully Updated"
+     render :edit
+    end
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,4 @@ Rails.application.routes.draw do
   root :to => 'homes#top'
   get '/top'=>'homes#top'
   resources :books
-  patch 'books/:id' => 'books#update', as: 'update_book'
-
-
 end
